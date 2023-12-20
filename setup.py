2449e3b8dfbf3849b111caee038c2e96008b8821
milc <milan@stubljar.com>
2023-12-20 09:50:24 +0100

    Revised.

--- setup.py
+++ setup.py
@@ -1,125 +1,44 @@
-import glob
-import itertools
 import os
+from distutils.core import setup
+from setuptools import find_packages
 
-# This is getting ridiculous
+# User-friendly description from README.md
+current_directory = os.path.dirname(os.path.abspath(__file__))
 try:
-    from pip._internal.req import parse_requirements
-    from pip._internal.network.session import PipSession
-except ImportError:
-    try:
-        from pip._internal.req import parse_requirements
-        from pip._internal.download import PipSession
-    except ImportError:
-        from pip.req import parse_requirements
-        from pip.download import PipSession
+    with open(os.path.join(current_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except Exception:
+    long_description = ''
 
-
-def setup_requirements(
-        patterns=[
-            'requirements.txt', 'requirements/*.txt', 'requirements/*.pip'
-        ],
-        combine=True):
-    """
-    Parse a glob of requirements and return a dictionary of setup() options.
-    Create a dictionary that holds your options to setup() and update it using this.
-    Pass that as kwargs into setup(), viola
-
-    Any files that are not a standard option name (ie install, tests, setup) are added to extras_require with their
-    basename minus ext. An extra key is added to extras_require: 'all', that contains all distinct reqs combined.
-
-    Keep in mind all literally contains `all` packages in your extras.
-    This means if you have conflicting packages across your extras, then you're going to have a bad time.
-    (don't use all in these cases.)
-
-    If you're running this for a Docker build, set `combine=True`.
-    This will set `install_requires` to all distinct reqs combined.
-
-    Example:
-
-    >>> import setuptools
-    >>> _conf = dict(
-    ...     name='mainline',
-    ...     version='0.0.1',
-    ...     description='Mainline',
-    ...     author='Trevor Joynson ',
-    ...     url='https://trevor.joynson.io',
-    ...     namespace_packages=['mainline'],
-    ...     packages=setuptools.find_packages(),
-    ...     zip_safe=False,
-    ...     include_package_data=True,
-    ... )
-    >>> _conf.update(setup_requirements())
-    >>> # setuptools.setup(**_conf)
-
-    :param str pattern: Glob pattern to find requirements files
-    :param bool combine: Set True to set install_requires to extras_require['all']
-    :return dict: Dictionary of parsed setup() options
-    """
-    session = PipSession()
-
-    # Handle setuptools insanity
-    key_map = {
-        'requirements': 'install_requires',
-        'install': 'install_requires',
-        'tests': 'tests_require',
-        'setup': 'setup_requires',
-    }
-    ret = {v: set() for v in key_map.values()}
-    extras = ret['extras_require'] = {}
-    all_reqs = set()
-
-    files = [glob.glob(pat) for pat in patterns]
-    files = itertools.chain(*files)
-
-    for full_fn in files:
-        # Parse
-        reqs = {
-            str(r.req)
-            for r in parse_requirements(full_fn, session=session)
-            # Must match env marker, eg:
-            #   yarl ; python_version >= '3.0'
-            if r.match_markers()
-        }
-        all_reqs.update(reqs)
-
-        # Add in the right section
-        fn = os.path.basename(full_fn)
-        barefn, _ = os.path.splitext(fn)
-        key = key_map.get(barefn)
-
-        if key:
-            ret[key].update(reqs)
-            extras[key] = reqs
-
-        extras[barefn] = reqs
-
-    if 'all' not in extras:
-        extras['all'] = list(all_reqs)
-
-    if combine:
-        extras['install'] = ret['install_requires']
-        ret['install_requires'] = list(all_reqs)
-
-    def _listify(dikt):
-        ret = {}
-
-        for k, v in dikt.items():
-            if isinstance(v, set):
-                v = list(v)
-            elif isinstance(v, dict):
-                v = _listify(v)
-            ret[k] = v
-
-        return ret
-
-    ret = _listify(ret)
-
-    return ret
-
-
-__all__ = ['setup_requirements']
-
-if __name__ == '__main__':
-    reqs = setup_requirements()
-    print(reqs)
+setup(
+	# Name of the package 
+	name='get-bs-ecb',
+	# Packages to include into the distribution 
+	packages=find_packages('.'),
+	# Start with a small number and increase it with 
+	# every change you make https://semver.org 
+	version='2.0.0',
+	# Chose a license from here: https: // 
+	# help.github.com / articles / licensing - a - 
+	# repository. For example: MIT 
+	license='MIT',
+	# Short description of your library 
+	description='BSI Exchange Rates Extractor',
+	# Long description of your library 
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	# Your name 
+	author='Milan Štubljar',
+	# Your email 
+	author_email='milan@stubljar.com',
+	# Either the link to your github or to your website 
+	url='https://github.com/milcs',
+	# Link from which the project can be downloaded 
+	download_url='https://github.com/milcs/get-bs-ecb.git',
+	# List of keywords 
+	keywords=[],
+	# List of packages to install with this one 
+	install_requires=[],
+	# https://pypi.org/classifiers/ 
+	classifiers=[]
+)
